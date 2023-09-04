4a930d7d49ab18127ace21385cf7fd74edd2e430
mityaku <144018015+mityaku@users.noreply.github.com>
2023-09-04 12:02:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((10, 600))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():  #this means its running yo
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("white") #ho ho ho merru chritysalma
+
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
